e44459f5c1e07e5953221b764212593a6ae5e50a
Fangyang Shen <shenfy@oppentech.com>
2018-08-25 12:01:08 +0800

    Addex texture2D function taking pixel coords as input.
    Change-Id: Ie82eebf92c86ec7a0fd1979e7741aadb4359bc2a

--- texfunc.py
+++ texfunc.py
@@ -23,7 +23,25 @@ def texture2D(img, uv):
         + f01 * (1 - dx) * dy + f11 * dx * dy
 
 @jit
-def texture2DNN(img, uv):
+def texture2D_px(img, xy):
+    h, w, c = img.shape
+    img_size = np.array([w, h])
+
+    lxy = np.floor(xy)
+    dx, dy = xy - lxy
+    x0, y0 = lxy
+    x1, y1 = lxy + 1
+
+    f00 = img[int(y0), int(x0), :]
+    f10 = img[int(y0), int(x1), :]
+    f01 = img[int(y1), int(x0), :]
+    f11 = img[int(y1), int(x1), :]
+
+    return f00 * (1 - dx) * (1 - dy) + f10 * dx * (1 - dy)\
+        + f01 * (1 - dx) * dy + f11 * dx * dy
+
+@jit
+def texture2D_nn(img, uv):
     h, w, c = img.shape
     img_size = np.array([w, h])
     texel = uv * img_size - 0.5
