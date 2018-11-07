7db0c733673b1875804cbedb67f7911893a9a1db
Fangyang Shen <shenfy@oppentech.com>
2018-11-07 17:42:36 +0800

    Added support for grayscale image sampling.
    Change-Id: Id38c4584c12916aab9ba18fec6afba6aee6d3649

--- texfunc.py
+++ texfunc.py
@@ -5,7 +5,7 @@ from PIL import Image
 # bilinear interpolation
 @jit
 def texture2D(img, uv):
-    h, w, c = img.shape
+    h, w = img.shape[:2]
     img_size = np.array([w, h])
     texel = np.array([uv[0], 1.0 - uv[1]]) * img_size - 0.5
 
@@ -14,17 +14,23 @@ def texture2D(img, uv):
     x0, y0 = lxy
     x1, y1 = lxy + 1
 
-    f00 = img[int(y0), int(x0), :]
-    f10 = img[int(y0), int(x1), :]
-    f01 = img[int(y1), int(x0), :]
-    f11 = img[int(y1), int(x1), :]
+    if len(img.shape) > 2:
+        f00 = img[int(y0), int(x0), :]
+        f10 = img[int(y0), int(x1), :]
+        f01 = img[int(y1), int(x0), :]
+        f11 = img[int(y1), int(x1), :]
+    else:
+        f00 = img[int(y0), int(x0)]
+        f10 = img[int(y0), int(x1)]
+        f01 = img[int(y1), int(x0)]
+        f11 = img[int(y1), int(x1)]
 
     return f00 * (1 - dx) * (1 - dy) + f10 * dx * (1 - dy)\
         + f01 * (1 - dx) * dy + f11 * dx * dy
 
 @jit
 def texture2D_px(img, xy):
-    h, w, c = img.shape
+    h, w = img.shape[:2]
     img_size = np.array([w, h])
 
     lxy = np.floor(xy)
@@ -32,22 +38,31 @@ def texture2D_px(img, xy):
     x0, y0 = lxy
     x1, y1 = lxy + 1
 
-    f00 = img[int(y0), int(x0), :]
-    f10 = img[int(y0), int(x1), :]
-    f01 = img[int(y1), int(x0), :]
-    f11 = img[int(y1), int(x1), :]
+    if len(img.shape) > 2:
+        f00 = img[int(y0), int(x0), :]
+        f10 = img[int(y0), int(x1), :]
+        f01 = img[int(y1), int(x0), :]
+        f11 = img[int(y1), int(x1), :]
+    else:
+        f00 = img[int(y0), int(x0)]
+        f10 = img[int(y0), int(x1)]
+        f01 = img[int(y1), int(x0)]
+        f11 = img[int(y1), int(x1)]
 
     return f00 * (1 - dx) * (1 - dy) + f10 * dx * (1 - dy)\
         + f01 * (1 - dx) * dy + f11 * dx * dy
 
 @jit
 def texture2D_nn(img, uv):
-    h, w, c = img.shape
+    h, w = img.shape[:2]
     img_size = np.array([w, h])
     texel = uv * img_size - 0.5
 
     xy = np.round(texel)
-    return img[int(xy[1]), int(xy[0]), :]
+    if len(img.shape) > 2:
+        return img[int(xy[1]), int(xy[0]), :]
+    else:
+        return img[int(xy[1]), int(xy[0])]
 
 if __name__ == '__main__':
     pass
