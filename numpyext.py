c98fbb4d67ec5efb945ab12e8e97cb586c5b466f
chubei <chubei@oppentech.com>
2018-11-14 15:32:23 +0800

    Fix bug in numpyext
    Change-Id: I5c45618b3f71c38fa35546c1f687accc98590426

--- numpyext.py
+++ numpyext.py
@@ -2,12 +2,12 @@ import numpy as np
 
 def to_homo(m, transpose=False):
   if transpose:
-    return _to_homo(m.T).T
+    return to_homo(m.T).T
   assert m.ndim == 2
   return np.vstack([m, np.ones((1, m.shape[1]), dtype=m.dtype)])
 
 def from_homo(m, transpose=False):
   if transpose:
-    return _from_homo(m.T).T
+    return from_homo(m.T).T
   assert m.ndim == 2
   return m[:-1, :] / m[-1, :]
